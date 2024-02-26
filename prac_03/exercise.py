eb593141d16b4b6fca94731cf4298b162e92fc9b
KyawThikeSanOliver <san.kyawthike@my.jcu.edu.au>
2024-02-26 18:08:52 +0800

    Update

---
+++ prac_03/exercise.py
@@ -0,0 +1,26 @@
+
+
+
+
+# file_path = "data.txt"
+# with open(file_path, 'r') as file:
+#     lines = file.readlines()
+#
+#
+# for line in lines:
+#     parts = line.strip().split(',')
+#     part = line.strip().split()
+#     name = part[0]
+#     year = int.part[1]
+#     price = part[2]
+#     print(name, year, price[0:-2])
+
+
+# zero division error
+# try:
+#     print(10/0)
+# except ZeroDivisionError:
+#     print("Division cannot be divided by zero")
+#
+
+ =
---
+++ prac_03/randoms.py
@@ -0,0 +1,22 @@
+"""
+1. What did you see on line 1?
+
+The command want to produce random numbers between 5 to 20
+The smallest number I could have seen was 6 and the largest is 19
+
+2.What did you see on line 2?
+
+The command want to produce random numbers from a range start from 3 and ends at 10 and step is 2
+The smallest - 3
+The largest - 9
+No it could not produce 4.
+
+3.What did you see on line 3?
+The command wants to produce random numbers the range of (2.3,5.5) or (5.5,2.5) depending on rounding
+2.499999999 and 5.49999
+
+
+"""
+
+import random
+print(random.randint(1, 100))
